7c0bc5608d7cc2c59049b1e216260ce2e9ebf862
Ray Cromwell <cromwellian@gmail.com>
2012-09-13 22:29:24 -0700

    push laod screen update

--- core/src/main/java/com/omegapoint/core/state/LoadGameState.java
+++ core/src/main/java/com/omegapoint/core/state/LoadGameState.java
@@ -4,6 +4,8 @@ import com.google.web.bindery.event.shared.EventBus;
 import com.omegapoint.core.events.ChangeStateEvent;
 import playn.core.Mouse;
 import playn.core.PlayN;
+import playn.core.ResourceCallback;
+import playn.core.Sound;
 import se.hiflyer.fettle.Action;
 import se.hiflyer.fettle.Arguments;
 import se.hiflyer.fettle.StateMachine;
@@ -27,13 +29,24 @@ public class LoadGameState extends AbstractGameState implements Action<GameState
     public void onTransition(GameState from, GameState to, String event, Arguments arguments, StateMachine<GameState, String> stateMachine) {
         ScreenStack screens = (ScreenStack) arguments.getFirst();
         screens.push(getScreen());
-        PlayN.mouse().setListener(new Mouse.Adapter() {
+        PlayN.assets().getSound("sounds/cybernoid2").addCallback(new ResourceCallback<Sound>() {
             @Override
-            public void onMouseUp(Mouse.ButtonEvent event) {
-                event.setPreventDefault(true);
-                PlayN.mouse().setListener(null);
+            public void done(Sound resource) {
+                eventBus.fireEvent(new ChangeStateEvent("intro"));
+            }
+
+            @Override
+            public void error(Throwable err) {
                 eventBus.fireEvent(new ChangeStateEvent("intro"));
             }
         });
+//        PlayN.mouse().setListener(new Mouse.Adapter() {
+//            @Override
+//            public void onMouseUp(Mouse.ButtonEvent event) {
+//                event.setPreventDefault(true);
+//                PlayN.mouse().setListener(null);
+//
+//            }
+//        });
     }
 }
