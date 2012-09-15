26b45cfe36318a763987f1b74a22737b1a80b14d
Ray Cromwell <cromwellian@gmail.com>
2012-09-15 15:27:16 -0700

    Tile Editor - left/right arrow and mouse wheel scrolls, right click erases tile

---
+++ core/src/main/java/com/omegapoint/core/state/TileEditorState.java
@@ -0,0 +1,29 @@
+package com.omegapoint.core.state;
+
+import com.omegapoint.core.screens.GameScreen;
+import com.omegapoint.core.inject.OmegaPointBaseModule;
+import se.hiflyer.fettle.Action;
+import se.hiflyer.fettle.Arguments;
+import se.hiflyer.fettle.StateMachine;
+
+import javax.inject.Inject;
+
+/**
+ *
+ */
+public class TileEditorState extends AbstractGameState implements Action<GameState,String> {
+    @Inject
+    public TileEditorState(GameScreen screen) {
+        super(screen);
+    }
+
+    @Override
+    public void onTransition(GameState from, GameState to, String event, Arguments arguments, StateMachine<GameState, String> gameStateStringStateMachine) {
+        OmegaPointBaseModule.ScreenStackImpl screens = (OmegaPointBaseModule.ScreenStackImpl) arguments.getFirst();
+        if (!screens.contains(getScreen())) {
+            screens.push(getScreen());
+        } else if (!screens.isTop(getScreen())) {
+            screens.popTo(getScreen(), screens.slide().down());
+        }
+    }
+}
